673ce6a6a2c867a24ceabe2bcf0b3dbbc717189b
TKSomar <tksomar@gmail.com>
2020-05-11 21:03:16 -0500

    Last problem - number 8

--- closures.js
+++ closures.js
@@ -61,23 +61,25 @@ let callJake = callFriend('Jake')
   Write a function called makeCounter that makes the following code work properly.
 */
 
-let startNum = 0;
+
 
 function makeCounter(){
-  function add(){
-    
+  let num = 0;
+  function increment(){
+    num++;
+    return num
   }
-  return add()
+  return increment;
 }
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -93,18 +95,25 @@ function makeCounter(){
 */
 
 function counterFactory(value) {
-  // Code here.
+  // var updatedValue;
 
   return {
-
+      inc:function() {
+        value++;
+        return value;
+      },
+      dec:function() {
+        value--;
+        return value;
+      }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -119,10 +128,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message(){
+    return `You're doing awesome, keep it up ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -150,9 +161,13 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+  publicMethod:function(){
+    return privateMethod()
+  },
   };
 })();
+
+module.publicMethod()
 
 
 
@@ -169,7 +184,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret:function(num){
+      secret += num
+      return secret
+    },
+    takeAwayFromSecret:function(num){
+      secret -= num
+      return secret
+    }
   };
 }
 
